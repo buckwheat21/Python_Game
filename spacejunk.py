4ef758da36e6141a11dadf921c1e1771843a20f8
buckwheat21 <76183350+buckwheat21@users.noreply.github.com>
2020-12-19 14:02:36 -0500

    Add files via upload

---
+++ spacejunk.py
@@ -0,0 +1,14 @@
+import pgzrun
+
+WIDTH = 1000     # width of the window
+
+HEIGHT = 600     # height of the window
+
+Red = (255, 0, 0) # Red, Green, Blue)
+Green = (0, 225, 0)
+Blue = (0,0,225)
+
+def draw ():
+    screen.fill(Red)
+
+pgzrun.go()
